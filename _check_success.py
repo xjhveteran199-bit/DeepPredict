4d516267e5c0a18554fde60107f47a5915ea9027
DeepPredict <deeppredict@example.com>
2026-03-27 09:43:16 +0800

    chore: 删除临时测试脚本和日志文件

--- _check_success.py
+++
@@ -1,6 +0,0 @@
-src = open(r'C:\Users\XJH\DeepPredict\deeppredict_web.py', encoding='utf-8').read()
-start = src.find('def on_file_upload(')
-end = src.find('\ndef plot_bland_altman(')
-func = src[start:end]
-idx = func.find('success =')
-print(func[idx:idx+1000])
